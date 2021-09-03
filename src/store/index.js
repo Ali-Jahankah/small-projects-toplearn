8d5688786f20e294d00cd691f902ee1a5ac43c67
Ali Jahankah <aral2@yahoo.com>
2021-09-03 19:13:40 +0100

    compose error fixed

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,8 @@ export const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
+      compose
   )
 );
