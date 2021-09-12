a463d605ffa4f1cb46b537c96493836d927c12f8
Ali Jahankah <aral2@yahoo.com>
2021-09-12 16:09:02 +0100

    Pagination finished

---
+++ src/componenets/Pagination.js
@@ -0,0 +1,29 @@
+import styles from "../css/paginate.css";
+import { range } from "lodash";
+const Pagination = ({ courses, currentPage, perPage, pageChange }) => {
+  const totalPages = Math.ceil(courses.length / perPage);
+  const pages = range(1, totalPages + 1);
+  console.log(pages);
+  if (totalPages === 1) return null;
+  return (
+    <div className={styles.paginate_container}>
+      <ul>
+        {pages.map((page) => {
+          return (
+            <li
+              key={page}
+              onClick={() => {
+                pageChange(page);
+              }}
+              className={currentPage === page ? styles.active : ""}
+            >
+              {page}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/componenets/paginate.js
+++ src/componenets/paginate.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
 export const paginate = (courses, currentPage, perPage) => {
-  const courseIndex = (1 - currentPage) * perPage;
+  const courseIndex = (currentPage - 1) * perPage;
   return _(courses).slice(courseIndex).take(perPage).value();
 };
